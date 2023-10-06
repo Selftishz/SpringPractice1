8b7f549071969a49a8ac9fc6adbb74358659db9b
Selftishz <art2sys048@gmail.com>
2023-10-06 22:06:17 +0300

    Beans and Stereotype Annotations (Component)

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "main")
+public class ProjectConfig {
+}
